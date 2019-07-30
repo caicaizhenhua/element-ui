2d5dfc25b474688b26622998758e62a27d5711ad
caicaizhenhua <caizhenhua@n-set.com.cn>
2019-07-30 09:23:02 +0800

    新建登录分支，新建登录组件配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      path: '/',
+      name: 'login',
+      component: Login
     }
   ]
 })
